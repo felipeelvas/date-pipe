f8ceb2df31cb4443aa30b342dc0cb8907c08f9a6
Felipe Elvas Barbosa <felipeelvas@gmail.com>
2024-08-03 22:27:40 -0300

    Formato ISO 8601 date e etc..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'date-pipe';
+export class AppComponent implements OnInit{
+  minhaData: string = '2024-08-03T22:22:00.000Z';
+
+  ngOnInit() {
+    console.log('Data Convertida para Região: ', new Date(this.minhaData))
+    console.log('Data UTC-0:', new Date(this.minhaData).toUTCString())
+  }
 }
