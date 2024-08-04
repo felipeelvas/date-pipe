153838192afa61f8a879567ddbf75e45dbb9dcdc
Felipe Elvas Barbosa <felipeelvas@gmail.com>
2024-08-04 13:24:38 -0300

    Formatações datas: dia-mes-ano-hora-minuto-period-week

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit{
-  minhaData: string = '2024-08-03T22:22:00.000Z';
+  minhaData: string = '2024-08-04T13:14:00.000';
+  minhaDataObj: Date = new Date('2024-08-03T22:22:00.000Z');
 
   ngOnInit() {
     console.log('Data Convertida para Região: ', new Date(this.minhaData))
     console.log('Data UTC-0:', new Date(this.minhaData).toUTCString())
+
+    console.log('Timestamp', new Date(this.minhaData).getTime() )
+
+    console.log(this.minhaDataObj)
+    console.log(this.minhaDataObj)
   }
 }
