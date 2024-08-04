6a7a95b6fa30118a7cfc52c27d499b8bf6e2656e
Felipe Elvas Barbosa <felipeelvas@gmail.com>
2024-08-04 18:40:42 -0300

    datePipeConfig and LocaleID

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,19 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DATE_PIPE_DEFAULT_OPTIONS, DatePipeConfig } from '@angular/common';
+
+import  localePt  from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+const datePipeConfig: DatePipeConfig = {
+      dateFormat: "'Data: 'dd/MM/YYYY",
+      timezone: '+0000'
+}
+
+registerLocaleData(localePt, 'pt-BR' );
 
 @NgModule({
   declarations: [
@@ -12,7 +23,16 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt_BR' 
+    },
+    {
+      provide: DATE_PIPE_DEFAULT_OPTIONS,
+      useValue: datePipeConfig
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
