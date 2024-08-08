df9881707dbb8b4d34deee42c1fe3e7ab31f6cc7
felipe <felipe@felipe.elvas>
2024-08-08 10:33:59 -0300

    currency pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { LOCALE_ID, NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,6 +8,7 @@ import { DATE_PIPE_DEFAULT_OPTIONS, DatePipeConfig } from '@angular/common';
 import  localePt  from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
 import { DecimalPipeComponent } from './decimal-pipe/decimal-pipe.component';
+import { CurrencyPipeComponent } from './currency-pipe/currency-pipe.component';
 
 const datePipeConfig: DatePipeConfig = {
       dateFormat: "'Data: 'dd/MM/YYYY",
@@ -19,21 +20,19 @@ registerLocaleData(localePt, 'pt-BR' );
 @NgModule({
   declarations: [
     AppComponent,
-    DecimalPipeComponent
+    DecimalPipeComponent,
+    CurrencyPipeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [
-    {
-      provide: LOCALE_ID,
-      useValue: 'pt_BR' 
-    },
-    {
-      provide: DATE_PIPE_DEFAULT_OPTIONS,
-      useValue: datePipeConfig
-    }
+    { provide: LOCALE_ID, useValue: 'pt_BR' },
+
+    {provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+
+    {provide: DATE_PIPE_DEFAULT_OPTIONS, useValue: datePipeConfig }
   ],
   bootstrap: [AppComponent]
 })
